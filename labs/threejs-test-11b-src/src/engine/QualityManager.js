4d32e0890eb68a6ce8690c1efc3714c383396cb6
raaioperations <alanhasan87@gmail.com>
2026-09-25 01:43:29 -0700

    Test11B: add QualityManager.js

---
+++ labs/threejs-test-11b-src/src/engine/QualityManager.js
@@ -0,0 +1,13 @@
+import {LIMITS} from '../config.js';
+export class QualityManager{
+  constructor(win=window){
+    this.win=win;
+    this.coarsePointer=win.matchMedia?.('(pointer: coarse)')?.matches??false;
+    const shortEdge=Math.min(win.innerWidth||0,win.innerHeight||0);
+    this.mobile=this.coarsePointer||shortEdge<=820;
+    this.maxDpr=this.mobile?LIMITS.mobileMaxDpr:LIMITS.desktopMaxDpr;
+    this.tier=this.mobile?'mobile':'desktop';
+  }
+  pixelRatio(){return Math.min(this.maxDpr,Math.max(1,this.win.devicePixelRatio||1));}
+  snapshot(){return Object.freeze({tier:this.tier,mobile:this.mobile,coarsePointer:this.coarsePointer,maxDpr:this.maxDpr,pixelRatio:this.pixelRatio()});}
+}
